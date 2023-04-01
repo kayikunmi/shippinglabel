82a2aa0e161397e3e15dd76c73a64c34e67aea55
KayikunmiMac <kayikunmiba.com>
2023-04-01 13:26:47 -0400

    now add to the qr

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ form.addEventListener('submit', (event) => {
   // Create the label template
   const labelTemplate = `
     <div class="label">
-      <h2>Shipping Label</h2>
+      <h2>Label Information </h2>
       <p>From: ${from}</p>
       <p>To: ${to}</p>
       <div id="qr-code"></div>
@@ -32,21 +32,21 @@ form.addEventListener('submit', (event) => {
   const qrCode = new QRious({
     element: qrCodeContainer,
     value: `${from}, ${to}`,
-    size: 150
+    size: 150 // Adjust the size of the QR code
   });
 
   // Display the QR code on the HTML page
-  const qrCodeImg = document.createElement('img');
-  qrCodeImg.src = qrCode.toDataURL();
-  qrCodeContainer.appendChild(qrCodeImg);
+  const qrCodeImage = new Image();
+  qrCodeImage.src = qrCode.toDataURL();
+  qrCodeContainer.appendChild(qrCodeImage);
 
   // Convert the label container to a PDF and download it
   const pdf = new jsPDF();
   pdf.fromHTML(labelContainer, 15, 15, {}, () => {
-    const qrCodeWidth = 80;
-    const qrCodeHeight = 80;
-    const x = (pdf.internal.pageSize.getWidth() / 2) - (qrCodeWidth / 2);
-    const y = 75;
+    const qrCodeWidth = 80; // Set the width of the QR code
+    const qrCodeHeight = 80; // Set the height of the QR code
+    const x = (pdf.internal.pageSize.getWidth() / 2) - (qrCodeWidth / 2); // Calculate the center position
+    const y = 75; // Set the y position of the QR code
     pdf.addImage(qrCode.toDataURL(), 'JPEG', x, y, qrCodeWidth, qrCodeHeight);
     pdf.save('shipping-label.pdf');
   });
