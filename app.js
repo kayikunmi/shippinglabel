5be2b7e728d369655cfe546ce58357cfdd8c88c4
KayikunmiMac <kayikunmiba.com>
2023-04-01 15:05:20 -0400

    decent

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // Get the form elements
 const form = document.querySelector('form');
-const fromInput = document.querySelector('#from');
-const toInput = document.querySelector('#to');
+const fromnameInput = document.querySelector('#fromname');
+const fromnumberInput = document.querySelector('#fromnumber');
+const fromaddressInput = document.querySelector('#fromaddress');
+const tonameInput = document.querySelector('#toname');
+const tonumberInput = document.querySelector('#tonumber');
+const toaddressInput = document.querySelector('#toaddress');
+const itemsInput = document.querySelector('#items');
+const noteInput = document.querySelector('#note');
+
 
 // Get the label container element
 const labelContainer = document.querySelector('#label-container');
@@ -11,15 +18,27 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   // Get the form data
-  const from = fromInput.value;
-  const to = toInput.value;
+  const fromname = fromnameInput.value;
+  const fromnumber = fromnumberInput.value;
+  const fromaddress = fromaddressInput.value;
+  const toname = tonameInput.value;
+  const tonumber = tonumberInput.value;
+  const toaddress = toaddressInput.value;
+  const items = itemsInput.value;
+  const note = noteInput.value;
 
   // Create the label template
   const labelTemplate = `
     <div class="label">
       <h2>Label Information </h2>
-      <p>From: ${from}</p>
-      <p>To: ${to}</p>
+      <p>Sender Name: ${fromname}</p>
+      <p>Sender Number: ${fromnumber}</p>
+      <p>Sender Address: ${fromaddress}</p>
+      <p>Receiver Name: ${toname}</p>
+      <p>Receiver Number: ${tonumber}</p>
+      <p>Receiver Address: ${toaddress}</p>
+      <p>Items: ${items}</p>
+      <p>Note: ${note}</p>
       <div id="qr-code"></div>
     </div>
   `;
@@ -31,8 +50,8 @@ form.addEventListener('submit', (event) => {
   const qrCodeContainer = document.querySelector('#qr-code');
   const qrCode = new QRious({
     element: qrCodeContainer,
-    value: `${from}, ${to}`,
-    size: 150 // Adjust the size of the QR code
+    value: `${fromname},${fromnumber},${fromaddress},${toname},${tonumber},${toaddress}${items},${note}`,
+    size: 80 // Adjust the size of the QR code
   });
 
   // Display the QR code on the HTML page
@@ -42,11 +61,11 @@ form.addEventListener('submit', (event) => {
 
   // Convert the label container to a PDF and download it
   const pdf = new jsPDF();
-  pdf.fromHTML(labelContainer, 15, 15, {}, () => {
+  pdf.fromHTML(labelContainer, 10, 10, {}, () => {
     const qrCodeWidth = 80; // Set the width of the QR code
     const qrCodeHeight = 80; // Set the height of the QR code
     const x = (pdf.internal.pageSize.getWidth() / 2) - (qrCodeWidth / 2); // Calculate the center position
-    const y = 75; // Set the y position of the QR code
+    const y = 100; // Set the y position of the QR code
     pdf.addImage(qrCode.toDataURL(), 'JPEG', x, y, qrCodeWidth, qrCodeHeight);
     pdf.save('shipping-label.pdf');
   });
